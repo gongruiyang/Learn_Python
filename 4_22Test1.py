54a69b4682be454567535a974f195813c804a5ca
gongruiyang <1252574653@qq.com>
2020-04-24 10:45:14 +0800

    日常练习

---
+++ 4_22Test1.py
@@ -0,0 +1,9 @@
+print("***********************************")
+temp = input("猜一个数字：")
+guess = int(temp)
+if guess == 8:
+    print("6666666\n")
+    print("猜对了！！！\n")
+else:
+    print("NONONO\n")
+print("bye\n")
